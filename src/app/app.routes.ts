bcb368579be72a36efbe2963589f69f7b3a39e15
Zuleidy Yaruro <49843884+zuleidyyaruro@users.noreply.github.com>
2025-06-24 16:17:51 -0500

    rutas y home component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'home', component: HomePageComponent },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: '**', redirectTo: 'home' }
+];
---
+++ src/app/pages/home-page/home-page.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { Card } from '../../interfaces/card.interface';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-home-page',
+  templateUrl: './home-page.component.html',
+  styleUrls: ['./home-page.component.css'],
+  standalone: true,
+  imports: [RouterLink]
+})
+export class HomePageComponent {
+
+  cards: Card[] = [
+    {
+      title: 'Cursos actualizados',
+      description: 'Contenido alineado a las últimas tendencias y tecnologías.',
+    },
+    {
+      title: 'Instructores expertos',
+      description: 'Aprende de profesionales con experiencia en la industria.',
+    },
+    {
+      title: 'Acceso a recursos exclusivos',
+      description: 'Materiales, herramientas y guías para potenciar tu aprendizaje.',
+    }
+  ]
+
+}
