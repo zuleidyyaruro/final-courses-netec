cc60a8b71c94097f56ac241887e0e0c6a1e80eb1
Zuleidy Yaruro <zvyaruro@gmail.com>
2025-06-25 09:51:39 -0600

    added routerLinkActive

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,12 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css'],
   standalone: true,
-  imports: [RouterLink]
+  imports: [RouterLink, RouterLinkActive]
 })
 export class NavbarComponent implements OnInit {
 
